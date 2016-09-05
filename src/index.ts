c33fb84106e001316206bd744afabcec500c6022
Murhaf Sousli <murhafsousli@gmail.com>
2016-09-05 09:30:13 +0300

    migrate to rc6

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-export * from './share/share.component';
-export * from './share/share.model';
+import {NgModule} from '@angular/core';
+
+import {ShareButtons} from './share/share.component';
+import {ShareButton} from './share/share.model';
+
+@NgModule({
+    declarations: [
+        ShareButtons
+    ],
+    exports: [
+        ShareButtons
+    ]
+})
+export class ShareModule{
+
+}
+
+export {
+    ShareButtons,
+    ShareButton
+    }
