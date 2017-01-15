1b75ed8bb0f00d6ea5083d926a8dfea89b87cd95
Tine Kondo <kondotine@gmail.com>
2017-01-15 09:36:06 +0100

    chore(demo): update demo app to use built 'ng2-sharebuttons' package from `dist/` folder
    - add a gulp task `link` to create a symlink from 'ng2-sharebuttons' package to `dist/` folder
    - add a dependency to 'ng2-sharebuttons' in the demo project
    - add a `postinstall` hook to `demo/package.json` to automatically use the 'linked' version instead of the one from NPM
    - fix `baseUrl` in `demo/src/index.html`
    - fix logos links in HeaderComponent

--- demo/src/app/header/header.component.ts
+++ demo/src/app/header/header.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  ngShareButtonLogo = prefixRepo('../../assets/img/logo.svg');
-  fbLogo = prefixRepo('../../assets/img/share/facebook.svg');
-  twttLogo = prefixRepo('../../assets/img/share/twitter.svg');
-  pintLogo = prefixRepo('../../assets/img/share/pinterest.svg');
-  stumbleLogo = prefixRepo('../../assets/img/share/stumbleupon.svg');
-  googleLogo = prefixRepo('../../assets/img/share/google-plus.svg');
-  tumblrLogo = prefixRepo('../../assets/img/share/tumblr.svg');
-  redditLogo = prefixRepo('../../assets/img/share/reddit.svg');
-  inLogo = prefixRepo('../../assets/img/share/linkedin.svg');
+  ngShareButtonLogo = '../../assets/img/logo.svg';
+  fbLogo = '../../assets/img/share/facebook.svg';
+  twttLogo = '../../assets/img/share/twitter.svg';
+  pintLogo = '../../assets/img/share/pinterest.svg';
+  stumbleLogo = '../../assets/img/share/stumbleupon.svg';
+  googleLogo = '../../assets/img/share/google-plus.svg';
+  tumblrLogo = '../../assets/img/share/tumblr.svg';
+  redditLogo = '../../assets/img/share/reddit.svg';
+  inLogo = '../../assets/img/share/linkedin.svg';
 
   constructor() { }
 
@@ -23,6 +23,3 @@ export class HeaderComponent implements OnInit {
   }
 
 }
-var prefixRepo = (path) => {
-  return  'ng2-sharebuttons' + path;
-};
