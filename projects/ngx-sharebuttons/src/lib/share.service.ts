c4b1f08fff4f59fd39e76df23c7464980fc21430
Murhaf Sousli <murhafsousli@gmail.com>
2020-05-17 03:24:04 +0200

    feat(core): Custom share method, closes #449

--- projects/ngx-sharebuttons/src/lib/share.service.ts
+++ projects/ngx-sharebuttons/src/lib/share.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable, Optional } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { BehaviorSubject } from 'rxjs';
-import { ShareButtonsConfig, SHARE_BUTTONS_CONFIG } from './share.models';
+import { IShareButton, SHARE_BUTTONS_CONFIG, ShareButtonsConfig, SharerMethod } from './share.models';
 import { SHARE_BUTTONS } from './share.defaults';
 import { mergeDeep } from './utils';
 
@@ -11,6 +12,10 @@ export class ShareService {
 
   /** Global config that applies on all share buttons in the app */
   config: ShareButtonsConfig = {
+    sharerMethod: SharerMethod.Anchor,
+    sharerTarget: '_blank',
+    windowObj: this._document.defaultView,
+    windowFuncName: 'open',
     prop: SHARE_BUTTONS,
     theme: 'default',
     include: [],
@@ -26,7 +31,7 @@ export class ShareService {
   /** Stream that emits when config changes */
   config$ = new BehaviorSubject(this.config);
 
-  constructor(@Optional() @Inject(SHARE_BUTTONS_CONFIG) config: ShareButtonsConfig) {
+  constructor(@Optional() @Inject(SHARE_BUTTONS_CONFIG) config: ShareButtonsConfig, @Inject(DOCUMENT) private _document: any) {
     if (config) {
       this.setConfig(config);
     }
@@ -40,7 +45,7 @@ export class ShareService {
   }
 
   get windowSize() {
-    return `width=${this.config.windowWidth}, height=${this.config.windowHeight}`;
+    return `width=${ this.config.windowWidth }, height=${ this.config.windowHeight }`;
   }
 
   setConfig(config: ShareButtonsConfig) {
