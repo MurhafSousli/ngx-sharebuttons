62159035be9f3a9bfdcfb60b086aa702e96e235a
Murhaf Sousli <murhafsousli@gmail.com>
2022-06-07 15:16:34 +0200

    Make test happy

--- projects/ngx-sharebuttons-demo/karma.conf.js
+++ projects/ngx-sharebuttons-demo/karma.conf.js
@@ -15,11 +15,6 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-sharebuttons-demo'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
